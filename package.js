802092fb16f089f0da9b5ba9c163f81e071328d0
bubblevuofficial-ops <saifchishtivuofficial@gmail.com>
2026-09-25 09:45:23 +0500

    Add files via upload

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "mahfooz-md-whatsapp-bot",
+  "version": "2.0.0",
+  "description": "Multi-Device WhatsApp Bot & Management Panel by Saif Chishti",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.13",
+    "axios": "^1.7.9",
+    "express": "^4.21.2",
+    "pino": "^9.5.0",
+    "qrcode": "^1.5.4",
+    "qrcode-terminal": "^0.12.0",
+    "socket.io": "^4.8.1"
+  }
+}
